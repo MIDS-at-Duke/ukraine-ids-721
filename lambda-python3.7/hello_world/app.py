580218a8cce85fc71e45ae46d9025fb064feec40
Haoliang Jiang <90075179+hj151mids@users.noreply.github.com>
2022-04-23 12:49:30 -0400

    Update app.py

--- lambda-python3.7/hello_world/app.py
+++ lambda-python3.7/hello_world/app.py
@@ -2,9 +2,13 @@ import datetime
 import zipfile
 import pandas as pd
 import os
+import boto3
+import io
+import json, subprocess
 
-os.environ['KAGGLE_USERNAME'] = "haoliangjiang1205"
-os.environ['KAGGLE_KEY'] = "b85cdfe8b2c0cf3e91ad16c5f1e01bf1"
+
+os.environ['KAGGLE_USERNAME'] = 'haoliangjiang1205'
+os.environ['KAGGLE_KEY'] = "ac4e3ccf40f9b2359f39a47317af2d20"
 os.environ['KAGGLE_CONFIG_DIR'] = '/tmp'
 
 if not os.path.exists('/tmp'):
@@ -41,10 +45,37 @@ def download_yesterday_data(api):
     if os.path.exists('/tmp/'+get_date_for_file()+'_UkraineCombinedTweetsDeduped.csv.gzip'):
         print('exists')
     #read part of the data or push the gzip to the s3
-    train = pd.read_csv('/tmp/'+get_date_for_file()+'_UkraineCombinedTweetsDeduped.csv.gzip', compression="gzip", nrows=10, index_col = 0)
-    print(train.head())
-    print('Done')
+    train = pd.read_csv('/tmp/'+get_date_for_file()+'_UkraineCombinedTweetsDeduped.csv.gzip', compression="gzip", index_col = 0)
+    return train
+def write_to_s3(data):
+    #normally you don't need to pass in access key, you can give lambda the permission
+    """write data to s3 bucket"""
+    AWS_S3_BUCKET = os.getenv("721finalproj")
+    AWS_ACCESS_KEY_ID = os.getenv("AKIAVWNNMDWO65JSPF7U")
+    AWS_SECRET_ACCESS_KEY = os.getenv("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")
 
+    s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    )
+    with io.StringIO() as csv_buffer:
+        data.to_csv(csv_buffer, index=False)
+        response = s3_client.put_object(
+            Bucket=AWS_S3_BUCKET, Key="data.csv", Body=csv_buffer.getvalue()
+            )
+
+        status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+        if status == 200:
+            return {
+               "message": "Successful Pushed data to S3.",
+                "status": status,
+            }
+        else:
+            return {
+                "message": "Was not successful in pushing data to S3.",
+                "status": status,
+            }
 def lambda_handler(event, context):
-    download_yesterday_data(api)
-    #push_to_s3()
+    data = download_yesterday_data(api)
+    write_to_s3(data)
